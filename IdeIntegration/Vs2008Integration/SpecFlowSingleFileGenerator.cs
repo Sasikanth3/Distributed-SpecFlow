5da16cd697e4c300b1e00862333c3187ccf05676
Sasikanth Raghava <v00d00childblues1@gmail.com>
2015-06-10 17:47:20 +0530

    .net 4.0 setup

--- IdeIntegration/Vs2008Integration/SpecFlowSingleFileGenerator.cs
+++ IdeIntegration/Vs2008Integration/SpecFlowSingleFileGenerator.cs
@@ -10,8 +10,8 @@ namespace TechTalk.SpecFlow.Vs2008Integration
 {
     [ComVisible(true)]
     [Guid("3C9CF10A-A9AB-4899-A0FB-4B3BE4A36C15")]
-    [CodeGeneratorRegistration(typeof(SpecFlowSingleFileGenerator), "C# XML Class Generator", vsContextGuids.vsContextGuidVCSProject, GeneratesDesignTimeSource = true)]
-    [CodeGeneratorRegistration(typeof(SpecFlowSingleFileGenerator), "VB XML Class Generator", vsContextGuids.vsContextGuidVBProject, GeneratesDesignTimeSource = true)]
+    [CodeGeneratorRegistration(typeof(SpecFlowSingleFileGenerator), "C# XML Class Generator", "{FAE04EC1-301F-11D3-BF4B-00C04F79EFBC}", GeneratesDesignTimeSource = true)]
+    [CodeGeneratorRegistration(typeof(SpecFlowSingleFileGenerator), "VB XML Class Generator", "{164B10B9-B200-11D0-8C61-00A0C91E29D5}", GeneratesDesignTimeSource = true)]
     [ProvideObject(typeof(SpecFlowSingleFileGenerator))]
     public class SpecFlowSingleFileGenerator : SpecFlowSingleFileGeneratorBase
     {
